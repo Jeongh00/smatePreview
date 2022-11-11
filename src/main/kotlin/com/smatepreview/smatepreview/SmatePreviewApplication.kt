544700bd943292a5095bf49a603657942a5dd5fe
Jeonghoo <austinan123@gmail.com>
2022-11-11 11:26:50 +0900

    git init

---
+++ src/main/kotlin/com/smatepreview/smatepreview/SmatePreviewApplication.kt
@@ -0,0 +1,11 @@
+package com.smatepreview.smatepreview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmatePreviewApplication
+
+fun main(args: Array<String>) {
+    runApplication<SmatePreviewApplication>(*args)
+}
---
+++ src/test/kotlin/com/smatepreview/smatepreview/SmatePreviewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.smatepreview.smatepreview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmatePreviewApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
