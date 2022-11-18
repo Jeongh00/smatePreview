bee8da711a0efb330a9702c52b1d0b212bc414e1
Jeonghoo <austinan123@gmail.com>
2022-11-18 18:12:09 +0900

    [feat]making dockerfiles

--- src/main/kotlin/com/smatepreview/smatepreview/controller/RestTemplateController.kt
+++ src/main/kotlin/com/smatepreview/smatepreview/controller/RestTemplateController.kt
@@ -12,23 +12,15 @@ import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.DefaultUriBuilderFactory
 import java.util.*
 
-
 @RestController
 // var ==> variable
 // val => value
 class RestTemplateController(
-    val restTemplate: RestTemplate,
-    val apiService: ApiService) {
+    val apiService: ApiService,
+    val restTemplate: RestTemplate) {
 
     @PostMapping("/resttemplate")
-//    @ApiOperation(value = "공공 데이터 저장", notes = "공공데이터 연결 후 저장")
-//    @ApiResponses({
-//        @ApiResponse(responseCode = "500", description = "서버 예외")
-//    })
-    fun restTemplateTest(): String {
-
-        val header = HttpHeaders()
-        header.contentType = MediaType.APPLICATION_JSON
+    fun restTemplateTest() {
 
         val request = RequestDto(
             listOf(
@@ -45,31 +37,7 @@ class RestTemplateController(
             )
         )
 
-        val entity: HttpEntity<*> = HttpEntity<RequestDto>(request, header)
-        val url = "http://api.odcloud.kr/api/nts-businessman/v1/validate"
-
-        val defaultUriBuilderFactory = DefaultUriBuilderFactory("http://api.odcloud.kr/api/nts-businessman/v1/validate")
-        defaultUriBuilderFactory.encodingMode = DefaultUriBuilderFactory.EncodingMode.NONE
-        val build =
-            defaultUriBuilderFactory.builder()
-                .queryParam("serviceKey", "VU2wvwgzw2TjYrvH%2FCdTQFmI7vn8rh4yNog9eSBCRPeg473G8%2F%2BMcwGZQbHluVTOyOSr2c0MoGUdFwOPm9a6mg%3D%3D")
-                .queryParam("returnType", "JSON")
-                .build()
-
-        val exchange = restTemplate.exchange(
-            build,
-            HttpMethod.POST,
-            entity,
-            ResponseDto::class.java
-        )
-
-        exchange.body?.also {
-            apiService.saveData(it)
-        }
-
-        println("exchange = $exchange")
-
-        return exchange.body.toString()
+        return apiService.gettingDataApi(restTemplate, request)
 
     }
 
@@ -84,4 +52,5 @@ class RestTemplateController(
     fun updateReview(@RequestBody request: RequestParam, @PathVariable("api_id") apiId: Long): Api {
         return apiService.updateById(apiId, request)
     }
+
 }
--- src/main/kotlin/com/smatepreview/smatepreview/service/ApiService.kt
+++ src/main/kotlin/com/smatepreview/smatepreview/service/ApiService.kt
@@ -2,18 +2,62 @@ package com.smatepreview.smatepreview.service
 
 import com.smatepreview.smatepreview.domain.Api
 import com.smatepreview.smatepreview.dto.Data
+import com.smatepreview.smatepreview.dto.RequestDto
 import com.smatepreview.smatepreview.dto.RequestParam
 import com.smatepreview.smatepreview.dto.ResponseDto
 import com.smatepreview.smatepreview.repository.ApiRepository
 import lombok.RequiredArgsConstructor
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.util.DefaultUriBuilderFactory
 import java.util.*
 import javax.transaction.Transactional
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-class ApiService (val apiRepository: ApiRepository) {
+class ApiService(
+    val apiRepository: ApiRepository,
+    val apiService: ApiService
+) {
+
+    @Transactional
+    fun gettingDataApi(restTemplate: RestTemplate, request: RequestDto) {
+
+        val header = HttpHeaders()
+        header.contentType = MediaType.APPLICATION_JSON
+
+        val entity: HttpEntity<*> = HttpEntity<RequestDto>(request, header)
+//        val url = "http://api.odcloud.kr/api/nts-businessman/v1/validate"
+
+        val defaultUriBuilderFactory =
+            DefaultUriBuilderFactory("http://api.odcloud.kr/api/nts-businessman/v1/validate")
+        defaultUriBuilderFactory.encodingMode = DefaultUriBuilderFactory.EncodingMode.NONE
+        val build =
+            defaultUriBuilderFactory.builder()
+                .queryParam(
+                    "serviceKey",
+                    "VU2wvwgzw2TjYrvH%2FCdTQFmI7vn8rh4yNog9eSBCRPeg473G8%2F%2BMcwGZQbHluVTOyOSr2c0MoGUdFwOPm9a6mg%3D%3D"
+                )
+                .queryParam("returnType", "JSON")
+                .build()
+
+        val exchange = restTemplate.exchange(
+            build,
+            HttpMethod.POST,
+            entity,
+            ResponseDto::class.java
+        )
+
+        exchange.body?.also {
+            apiService.saveData(it)
+        }
+
+    }
 
     @Transactional
     fun saveData(dto: ResponseDto) {
